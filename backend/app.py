3eca17b89136b93f61f455708b3d2e61d9e8e1f2
Elvira <ajdar.elvira@hotmail.com>
2026-03-21 22:12:25 +0100

    code in app.py

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.book import router as book_router
+
+app = FastAPI()
+
+# CORS för frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(book_router)
