080657a7e602a6d5e639254b6a606c6601a6e21b
Elvira <ajdar.elvira@hotmail.com>
2026-03-23 17:31:11 +0100

    small changes to make it work

--- backend/models/todo.py
+++ backend/models/todo.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class Book(Base):
-    __tablename__ = "books"
+class Todo(Base):
+    __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    author = Column(String, index=True)
+    description = Column(String, nullable=True)
